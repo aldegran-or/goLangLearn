236f0aea41179dde019ac241eb93161269852c9a
Evgen Surkov <aldegran@gmail.com>
2017-01-08 00:34:06 +0200

    Chapter 3 - lesson 3 VARS coomit1

--- golang-book/chapter3/bigBool.go
+++ golang-book/chapter3/bigBool.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println((true && false) || (false &&
-true) || !(false && false))
+	fmt.Println("(true && false) || (false && true) || !(false && false) =",(true && false) || (false && true) || !(false && false))
 	}
