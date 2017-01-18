6a7eedd7b29364d85307829e5fdfa0ada3347373
Evgen Surkov <aldegran@gmail.com>
2017-01-18 18:16:44 +0200

    Chapter 5 lesson 1 commit 3 Home Work

---
+++ golang-book/chapter5/divBy3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+func main(){
+	for i := 1; i <= 100; i++ {
+		if i % 3 == 0 {
+			fmt.Println(i)
+	/*	} else 	if i % 6 == 0 {
+			fmt.Println(i)
+		} else if i % 9 {
+			fmt.Println(i)*/
+		} 
+	} 
+}
