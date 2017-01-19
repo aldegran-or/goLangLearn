564b0df02d3363ce8c6669ec718ad7ce51a6e905
Evgen Surkov <aldegran@gmail.com>
2017-01-19 20:53:39 +0200

    Chapter 5 lesson 1 commit 4 Home Work

--- golang-book/chapter5/divBy3.go
+++ golang-book/chapter5/divBy3.go
@@ -5,10 +5,6 @@ func main(){
 	for i := 1; i <= 100; i++ {
 		if i % 3 == 0 {
 			fmt.Println(i)
-	/*	} else 	if i % 6 == 0 {
-			fmt.Println(i)
-		} else if i % 9 {
-			fmt.Println(i)*/
 		} 
 	} 
 }
---
+++ golang-book/chapter5/divBy3And5.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+func main(){
+	for i := 1; i <= 100; i++ {
+		if i % 3 == 0 {
+			if i % 5 == 0 {
+				fmt.Println(i, "Это число делится на 5 и 3")
+				} else { fmt.Println(i, "Это число делится на 3")
+					}
+			
+		} else if i % 5 == 0 {
+			fmt.Println(i, "Это число делится на 5")
+		} else { fmt.Println(i, "Это число не делится ни на 3 ни на 5")
+	} 
+}
+}
