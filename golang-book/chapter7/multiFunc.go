39723819f6e8ccc35e366fa09b1ffa66adc46021
Evgen Surkov <aldegran@gmail.com>
2017-02-08 21:54:12 +0200

    Chaper 7 lessons 1 commit 2 Function, multivarStdout

---
+++ golang-book/chapter7/multiFunc.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func add(args ...int) int {
+  total := 0
+  for _, v:= range args {
+    total = total + v
+  }
+  return total
+}
+
+func main() {
+  xs:=[]int{1,2,3,4,}
+  fmt.Println(add(xs...))
+  //  fmt.Println(add(1,2,3))
+}
